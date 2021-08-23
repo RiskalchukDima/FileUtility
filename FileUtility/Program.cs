2fc7a9e2b47c3efb163c33f40dd5bd9077465ebb
driskalchuk <driskalchuk@gmail.com>
2021-08-24 00:49:02 +0300

    write args

--- FileUtility/Program.cs
+++ FileUtility/Program.cs
@@ -7,6 +7,11 @@ namespace FileUtility
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
+			foreach (string arg in args)
+			{
+				Console.WriteLine(arg);
+			}
+			Console.ReadLine();
 		}
 	}
 }
